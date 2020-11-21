1f56cac68cf02ecd818ff0548337eabca16a8ec1
taufiqzaidong <58703430+taufiqzaidong@users.noreply.github.com>
2020-11-21 21:05:38 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:lab5_app/pages/choose_location.dart';
 import 'package:lab5_app/pages/home.dart';
 import 'package:lab5_app/pages/loading.dart';
 
+//Mohd Taufiq  bin Zaidong
+//195896
+
 void main() => runApp(MaterialApp(initialRoute: '/', routes: {
       '/': (context) => Loading(),
       '/home': (context) => Home(),
